5f6e07e61b6b3d13b125dbd416946c2154c43497
Oleh Kliuiko <oleh.kliuiko@selectoglobal.com>
2020-12-07 14:51:29 +0200

    create Search Page and add navigation

---
+++ lib/pages/search_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SearchPage extends StatefulWidget {
+  @override
+  _SearchPageState createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage>{
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            onPressed: () => Navigator.pop(context),
+            icon: Icon(Icons.arrow_back)),
+      ),
+      body: Center(
+        child: Column(
+           mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+             TextField(
+               controller: ,
+             )
+            ]),
+      ),
+    );
+  }
+}
